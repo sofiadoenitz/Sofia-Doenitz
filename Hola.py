42b25f2d76c49396de79217350dd7192a09db85b
sofiadoenitz <so.doenitz@duocuc.cl>
2025-04-15 13:19:13 -0400

    python 1ra clase

--- Hola.py
+++ Hola.py
@@ -5,11 +5,29 @@
 # print("Hola", nombre, "su edad es", edad)
 
 
+# print("ingrese 3 notas para sacar su promedio")
+# n1=int(input("ingrese su nota "))
+# n2=int(input("ingrese su nota "))
+# n3=int(input("ingrese su nota "))
+# prom=(n1+n2+n3)/3
+# print("su promedio es", prom)
 
-n1=int(input("ingrese un numero "))
-n2=int(input("ingrese un numero "))
-print("el resultado de la suma es", n1+n2)
+# if prom>=40:
+#     print("usted aprobo")
+# else: 
+#     print("usted reprobo")
+# verificacion de edad
 
+# edad=int(input("ingrese su edad "))
+
+# if edad>=18:
+#     print("usted es mayor de edad")
+# else:
+#     print("usted es menor de edad")
+
+# niño menor 12
+# adolescente entre 12 y 17
+# mayor de edad mas o igual a 18
 
 
 
