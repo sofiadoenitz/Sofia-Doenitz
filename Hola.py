f34bb05805be420262b7b5cd1a6ebec643bd8afe
sofiadoenitz <so.doenitz@duocuc.cl>
2025-04-15 11:54:25 -0400

    primer commit

---
+++ Hola.py
@@ -0,0 +1,15 @@
+# # declaracion de variable
+# nombre="link"
+# edad=88
+# # ejemplo de concatenacion
+# print("Hola", nombre, "su edad es", edad)
+
+
+
+n1=int(input("ingrese un numero "))
+n2=int(input("ingrese un numero "))
+print("el resultado de la suma es", n1+n2)
+
+
+
+
